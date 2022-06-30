16bd74db3407f7f313d404ff91ebca289f7063c0
Neivedhya <neivukc47@gmail.com>
2022-06-30 20:11:27 +0530

    completed rock paper scissors

--- script.js
+++ script.js
@@ -1,48 +1,129 @@
 let playerPoints = 0;
 let computerPoints = 0;
-game();
+
+let buttons = document.querySelectorAll(".container button");
+let playerSign = document.querySelector("#playerSign");
+let computerSign = document.querySelector("#computerSign");
+let modal = document.querySelector(".modal");
+let playAgainBtn = document.querySelector("#playAgainBtn");
+let overlay = document.querySelector("#overlay");
+let firstPara = document.querySelector("#firstPara");
+let scoreResult = document.querySelector("#scoreResult");
+let playerScore = document.querySelector("#playerScore");
+let computerScore = document.querySelector("#computerScore");
+let roundWinner = document.querySelector("#roundWinner");
+
+overlay.addEventListener("click",closeModal);
+playAgainBtn.addEventListener("click",restartGame);
+
+function closeModal()
+{
+    overlay.classList.remove("active");
+    modal.classList.remove("active");
+}
+function openModal()
+{
+    if(playerPoints == 5)
+    {
+        roundWinner.textContent = "You won!";
+    }
+    else
+    {
+        roundWinner.textContent = "Computer Won!";
+    }
+
+    overlay.classList.add("active");
+    modal.classList.add("active");
+}
+function restartGame()
+{
+    firstPara.textContent = "Choose your weapon";
+    playerPoints = 0;
+    computerPoints= 0;
+    playerScore.textContent = "PlayerScore: 0";
+    computerScore.textContent = "ComputerScore: 0";
+    playerSign.textContent = "❔";
+    computerSign.textContent = "❔";
+    scoreResult.textContent = " First to score 5 points wins the game"; 
+    closeModal();
+}
+buttons[0].addEventListener("click",function()
+{
+    conductRound("Rock",computerPlay());
+    playerSign.textContent = "✊";
+},);
+buttons[1].addEventListener("click",function()
+{
+    conductRound("Paper",computerPlay());
+    playerSign.textContent = "✋";
+},);
+buttons[2].addEventListener("click",function()
+{
+    conductRound("Scissors",computerPlay());
+    playerSign.textContent = "✌";
+},);
 function computerPlay()
 {
-    let arr = ["Rock","Paper","Scrissors"];
-    return arr[Math.floor(Math.random() * 3)];
+    let arr = ["Rock","Paper","Scissors"];
+    let selected = Math.floor(Math.random() * 3);
+    if(arr[selected] == "Rock")
+    {
+        computerSign.textContent = "✊";
+    }
+    if(arr[selected] == "Paper")
+    {
+        computerSign.textContent = "✋";
+    }
+    if(arr[selected] == "Scissors")
+    {
+        computerSign.textContent = "✌";
+    }
+    return arr[selected];
 }
 function conductRound(playerSelection,computerSelection)
 {
+    if(playerPoints==5 || computerPoints == 5)
+    {
+        openModal();
+        return;
+    }
     const playerSelectionLow = playerSelection.toLowerCase();
     const computerSelectionLow = computerSelection.toLowerCase();
+
     if(playerSelectionLow == computerSelectionLow)
     {
-        playerPoints++;
-        return "Draw, Play Again."
+        firstPara.textContent="Draw!";
+        scoreResult.textContent =playerSelection + " ties with " + computerSelection;
     }
     else if(playerSelectionLow=="rock" && computerSelectionLow=="scissors")
     {
         playerPoints++;
-        return "Player Won!";
+        firstPara.textContent = "Player Won!";
+        scoreResult.textContent = "Rock beats Scissors";
     }
     else if(playerSelectionLow=="paper" && computerSelectionLow=="rock")
     {
         playerPoints++;
-        return "Player Won!";
+        firstPara.textContent = "Player Won!";
+        scoreResult.textContent = "Paper beats Rock";
     }
     else if(playerSelectionLow=="scissors" && computerSelectionLow=="paper")
     {
         playerPoints++;
-        return "Player Won!";
+        firstPara.textContent = "player Won!";
+        scoreResult.textContent = "Scissors beats Paper";
     }
     else
     {
         computerPoints++;
-        return "Computer Won!";
+        firstPara.textContent = "Computer Won!";
+        scoreResult.textContent = computerSelection + " beats " + playerSelection;
     }
-}
-function game()
-{
-    for(let i =0;i<5;i++)
+    playerScore.textContent = "Player: " + playerPoints;
+    computerScore.textContent = "computer: " + computerPoints;
+
+    if(playerPoints==5 || computerPoints == 5)
     {
-        let playerselection = prompt("Enter your choice.")
-        let computerSelection = computerPlay();
-        console.log("Computer selection :" + computerSelection);
-        console.log(conductRound(playerselection,computerSelection));
+        openModal();
     }
 }
